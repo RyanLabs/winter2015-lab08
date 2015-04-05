37fe01a10bc981b48748da4133fe3d4640df6ac2
Ryan Sadio <ryan@sadio.ca>
2015-04-05 15:42:04 -0700

    Add users model

---
+++ application/models/Users.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryansadio
+ * Date: 15-04-05
+ * Time: 15:41
+ */
+class Users extends MY_Model {
+    public function __construct() {
+        parent::__construct('users', 'id');
+    }
+}
